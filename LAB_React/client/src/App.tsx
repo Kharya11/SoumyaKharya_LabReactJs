ff48993568ebab47bc6b35df467d1b20a486f85d
soumya <kharyasoumya2011@gmail.com>
2022-07-12 17:26:55 +0200

    ReactJS lab Commit

---
+++ LAB_React/client/src/App.tsx
@@ -0,0 +1,10 @@
+import ExpenseTracker from "./components/ExpenseTracker";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return ( 
+  <ExpenseTracker/>
+  );
+} 
+
+export default App;
